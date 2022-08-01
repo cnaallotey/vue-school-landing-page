76e4ee3e47e08cb58b56c4ad7617dca9e7c1897a
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-08-01 12:25:40 +0000

    add timer component

--- components/countdown.vue
+++ components/countdown.vue
@@ -1,96 +1,29 @@
 <template>
   <div class="w-full my-10">
     <div class="w-fit mx-auto flex items-center space-x-2">
-      <div class="flex flex-col items-center">
-        <div class="w-16 h-16 lg:w-24 lg:h-24 relative">
-          <div class="flex flex-col space-y-0.5 w-full h-full rounded-md overflow-hidden">
-            <div class="w-full h-1/2 bg-secondary-600"></div>
-            <div class="w-full h-1/2 bg-secondary-900"></div>
-          </div>
-          <div class="w-full absolute inset-0 h-full flex items-center justify-center">
-            <p class="text-4xl lg:text-6xl font-sans font-medium text-primary-500">
-              <!-- check if countdown has exceeded 0 -->
-              <span v-if="countdown.days > 0"
-                ><span v-if="countdown.days < 10">0</span>{{ countdown.days }}</span
-              >
-              <span v-else>00</span>
-            </p>
-          </div>
-        </div>
-        <p class="text-xs font-sans font-normal text-white mt-1">Days</p>
-      </div>
+      <Timer type="Days" :time="countdown.days" />
       <p
         class="text-secondary-500 font-sans text-3xl lg:text-5xl font-normal -mt-6"
         :class="blink && countdown.seconds < 0 ? 'invisible' : 'visible'"
       >
         :
       </p>
-      <div class="flex flex-col items-center">
-        <div class="w-16 h-16 lg:w-24 lg:h-24 relative">
-          <div class="flex flex-col space-y-0.5 w-full h-full rounded-md overflow-hidden">
-            <div class="w-full h-1/2 bg-secondary-600"></div>
-            <div class="w-full h-1/2 bg-secondary-900"></div>
-          </div>
-          <div class="w-full absolute inset-0 h-full flex items-center justify-center">
-            <p class="text-4xl lg:text-6xl font-sans font-medium text-primary-500">
-              <!-- check if countdown has exceeded 0 -->
-              <span v-if="countdown.hours > 0"
-                ><span v-if="countdown.hours < 10">0</span>{{ countdown.hours }}</span
-              >
-              <span v-else>00</span>
-            </p>
-          </div>
-        </div>
-        <p class="text-xs font-sans font-normal text-white mt-1">Hours</p>
-      </div>
+      <Timer type="Hours" :time="countdown.hours" />
       <p
         class="text-secondary-500 font-sans text-3xl lg:text-5xl font-normal -mt-6"
         :class="blink && countdown.seconds < 0 ? 'invisible' : 'visible'"
       >
         :
       </p>
-      <div class="flex flex-col items-center">
-        <div class="w-16 h-16 lg:w-24 lg:h-24 relative">
-          <div class="flex flex-col space-y-0.5 w-full h-full rounded-md overflow-hidden">
-            <div class="w-full h-1/2 bg-secondary-600"></div>
-            <div class="w-full h-1/2 bg-secondary-900"></div>
-          </div>
-          <div class="w-full absolute inset-0 h-full flex items-center justify-center">
-            <p class="text-4xl lg:text-6xl font-sans font-medium text-primary-500">
-              <!-- check if countdown has exceeded 0 -->
-              <span v-if="countdown.minutes > 0"
-                ><span v-if="countdown.minutes < 10">0</span>{{ countdown.minutes }}</span
-              >
-              <span v-else>00</span>
-            </p>
-          </div>
-        </div>
-        <p class="text-xs font-sans font-normal text-white mt-1">Minutes</p>
-      </div>
+
+      <Timer type="Minutes" :time="countdown.minutes" />
       <p
         class="text-secondary-500 font-sans text-3xl lg:text-5xl font-normal -mt-6"
         :class="blink && countdown.seconds < 0 ? 'invisible' : 'visible'"
       >
         :
       </p>
-      <div class="flex flex-col items-center">
-        <div class="w-16 h-16 lg:w-24 lg:h-24 relative">
-          <div class="flex flex-col space-y-0.5 w-full h-full rounded-md overflow-hidden">
-            <div class="w-full h-1/2 bg-secondary-600"></div>
-            <div class="w-full h-1/2 bg-secondary-900"></div>
-          </div>
-          <div class="w-full absolute inset-0 h-full flex items-center justify-center">
-            <p class="text-4xl lg:text-6xl font-sans font-medium text-primary-500">
-              <!-- check if countdown has exceeded 0 -->
-              <span v-if="countdown.seconds > 0"
-                ><span v-if="countdown.seconds < 10">0</span>{{ countdown.seconds }}
-              </span>
-              <span v-else>00</span>
-            </p>
-          </div>
-        </div>
-        <p class="text-xs font-sans font-normal text-white mt-1">Seconds</p>
-      </div>
+      <Timer type="Seconds" :time="countdown.seconds" />
     </div>
   </div>
 </template>
@@ -98,6 +31,7 @@
 <script setup>
 import { onMounted, reactive, ref } from "vue";
 import moment from "moment";
+import Timer from "./timer.vue";
 
 const countdown = reactive({ days: "", hours: "", minutes: "", seconds: "" });
 
@@ -116,7 +50,7 @@ onMounted(() => {
   //getting current date and time and finding the difference to specified data and time
   setInterval(function () {
     const x = new moment();
-    const y = new moment("2022-08-01T00:00:00+02:00");
+    const y = new moment("2022-09-01T00:00:00+02:00");
     countdown.days = moment.duration(y.diff(x)).days();
     countdown.hours = moment.duration(y.diff(x)).hours();
     countdown.minutes = moment.duration(y.diff(x)).minutes();
---
+++ components/timer.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+  type: String,
+  time: Number,
+});
+</script>
+
+<template>
+  <div>
+    <div class="flex flex-col items-center">
+      <div class="w-16 h-16 lg:w-24 lg:h-24 relative">
+        <div class="flex flex-col space-y-0.5 w-full h-full rounded-md overflow-hidden">
+          <div class="w-full h-1/2 bg-secondary-600"></div>
+          <div class="w-full h-1/2 bg-secondary-900"></div>
+        </div>
+        <div class="w-full absolute inset-0 h-full flex items-center justify-center">
+          <p class="text-4xl lg:text-6xl font-sans font-medium text-primary-500">
+            <!-- check if countdown has exceeded 0 -->
+            <span v-if="props.time > 0"
+              ><span v-if="props.time < 10">0</span>{{ props.time }}</span
+            >
+            <span v-else>00</span>
+          </p>
+        </div>
+      </div>
+      <p class="text-xs font-sans font-normal text-white mt-1">{{ props.type }}</p>
+    </div>
+  </div>
+</template>
